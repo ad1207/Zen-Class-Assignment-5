2fd1f89d924ff02f6f439f31afc8965467f8acde
Adithya R <59307022+ad1207@users.noreply.github.com>
2021-12-11 18:39:52 +0530

    Create Circle - OOP.js

--- Circle - OOP.js
+++ Circle - OOP.js
@@ -25,7 +25,7 @@ class Circle{
         return 2*Math.PI*this.radius
     }
 }
-let c = new Circle(1.0,'red)
+let c = new Circle(1.0,'red')
 console.log(c.toString())
 console.log(c.getArea())
 console.log(c.getCircumference())
